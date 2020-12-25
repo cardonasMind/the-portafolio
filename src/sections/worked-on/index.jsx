1744546dbd2b4b6490d10df4f08de5c58115dc0f
Diego Cardona <person.cardonadiego@gmail.com>
2020-12-25 18:58:40 -0500

    worked-on section more stylish

--- src/sections/worked-on/index.jsx
+++ src/sections/worked-on/index.jsx
@@ -1,16 +1,15 @@
 import react from "react";
 
+import "./index.css";
+
 import WorkCard from "./work-card";
 
 export default function WorkedOn() {
     return (
-        <div className="my-16 mx-16">
-            <h1 className="text-6xl mb-8 text-white border-b-4 border-white">I've worked on</h1>
+        <div className="worked-on">
+            <h1>I've worked on</h1>
             
-            <div className="grid grid-cols-4 gap-4">
-                <WorkCard />
-                <WorkCard />
-                <WorkCard />
+            <div className="worked-on__cards">
                 <WorkCard />
                 <WorkCard />
             </div>
