27443415e94940657be52feae7882c2a409bcaa0
Diego Cardona <person.cardonadiego@gmail.com>
2021-08-25 12:34:41 -0500

    Added See it button for external url and About it for the info of the project itself, also added svg icon external-link

--- src/sections/worked-on/work-card/index.jsx
+++ src/sections/worked-on/work-card/index.jsx
@@ -8,13 +8,18 @@ export default function WorkCard({ changeLightboxContent, toggleShowLightbox, im
             <h2>{title}</h2>
             <p>{short}</p>
             
-            <div className="worked-on__button"
-                onClick={() => {
-                    changeLightboxContent(children);
-                    toggleShowLightbox();
-                }}
-            >
-                <b>See it</b> <img src="/static/icons/enter.svg" />
+            <div className="worked-on__buttons">
+                <a className="see-it__button" href="https://www.google.com" target="_blank">
+                    <b>See it</b> <img src="/static/icons/external-link.svg" />
+                </a>
+                <div className="about-it__button"
+                    onClick={() => {
+                        changeLightboxContent(children);
+                        toggleShowLightbox();
+                    }}
+                >
+                    <p>About it</p> <img src="/static/icons/enter.svg" />
+                </div>
             </div>
         </div>
     )
