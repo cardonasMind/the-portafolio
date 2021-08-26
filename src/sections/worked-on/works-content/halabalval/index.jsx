6e223e888f00844d67583f1401309fec3661fe89
Diego Cardona <person.cardonadiego@gmail.com>
2021-08-25 19:02:48 -0500

    halabalval card in works-content is complete and better!

--- src/sections/worked-on/works-content/halabalval/index.jsx
+++ src/sections/worked-on/works-content/halabalval/index.jsx
@@ -12,14 +12,20 @@ export default function Halabalval({ toggleShowLightbox }) {
             
             <div id="halabalval-project__content">
                 <h1>♒️ HALABALVAL ♒️</h1>
-                <p>Halabalval is a countries quiz game which I have make to learn and refine my skills.</p> 
-
-                <p>This project uses <a href="https://restcountries.eu/" target="_blank">https://restcountries.eu/</a> API to generate every question and their answers. Doing this project, back then, I learnt new skills like Redux and SASS.</p>
                 
-                <p>Just go ahead and test it yourself!</p>
-                <b>And more importantly, check out the quality of the code by clicking on repo</b>
-                <img src="/static/images/sections/worked-on/halabalval/simple.png" />
-                <a href="https://halabalval.vercel.app/" target="_blank">click to Start!</a>
+                <p>Halabalval is just a random word, don´t pay to much attention to it.</p>
+
+                <p>Its a countries quiz game which uses a free and public REST API <a href="https://restcountries.eu/" target="_blank">https://restcountries.eu/</a> to get all the necessary data to every action (sort of) of the game.</p>
+
+                <p>For simplicity purposes I decided to just work with 20 countries. So in this case, the are only questions and answers about those 20.</p>
+
+                <p>At the beggining of the game and every time the human selects correct answer, a new question is generated. And for generate that question the API receives GET responses from the client (made using NextJS, which is a super-set of React) to generate the question, the answer itself and also to generate the other three wrong answers.</p>
+
+                <p>When the human selects the correct answer, human pass to the next question and gets 2 points. If human err the answer, game ENDDDSSS!</p>
+                
+                <h2>Types of question</h2>
+                
+                <p>There are 3 types of questions, go ahead and see it yourself!</p>
             </div>
         </div>
     )
