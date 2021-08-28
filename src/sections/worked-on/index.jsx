7cdb35080f68ceb18965a6b13fcc7045cb1e432d
Diego Cardona <person.cardonadiego@gmail.com>
2021-08-27 22:51:01 -0500

    Added Hippopotamus project and ordered what now is Projects showcase (before: I have worked on) from recent to older

--- src/sections/worked-on/index.jsx
+++ src/sections/worked-on/index.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 
 import WorkCard from "./work-card";
 
-import { Santiamen, Halabalval } from "./works-content";
+import { Santiamen, Halabalval, Hippopotamus } from "./works-content";
 
 export default class extends PureComponent {
     state = {
@@ -25,22 +25,31 @@ export default class extends PureComponent {
         return (
             <Fragment>
                 <div id="worked-on" style={{ display: showLightbox ? "none" : "block" }}>
-                    <h1 id="worked-on__title">I've worked on</h1>
+                    <h1 id="worked-on__title">Projects showcase</h1>
 
                     <div id="worked-on__cards">
                         <WorkCard changeLightboxContent={this.changeLightboxContent}
                             toggleShowLightbox={this.toggleShowLightbox} 
-                            image="/static/images/sections/worked-on/santiamen/cover.png" 
-                            title="Santiamén" short="Fast-food delivery platform"
-                            link="https://santiamen.ml/">
-                            <Santiamen toggleShowLightbox={this.toggleShowLightbox} />
+                            image="/static/images/sections/worked-on/hippopotamus/cover.png" 
+                            title="Hippopotamus 💬" short="Real-time chat."
+                            link="https://hippopotamus-client.vercel.app">
+                            <Hippopotamus toggleShowLightbox={this.toggleShowLightbox} />
                         </WorkCard>
+
                         <WorkCard changeLightboxContent={this.changeLightboxContent}
                             toggleShowLightbox={this.toggleShowLightbox} 
                             image="/static/images/sections/worked-on/halabalval/cover.png" 
                             title="♒️ HALABALVAL ♒️" short="The Countries Quiz Game"
                             link="https://halabalval.vercel.app/welcome">
                             <Halabalval toggleShowLightbox={this.toggleShowLightbox} />
+                        </WorkCard>
+
+                        <WorkCard changeLightboxContent={this.changeLightboxContent}
+                            toggleShowLightbox={this.toggleShowLightbox} 
+                            image="/static/images/sections/worked-on/santiamen/cover.png" 
+                            title="Santiamén" short="Fast-food delivery platform"
+                            link="https://santiamen.ml/">
+                            <Santiamen toggleShowLightbox={this.toggleShowLightbox} />
                         </WorkCard>
                     </div>
                 </div>
--- src/sections/worked-on/works-content/index.jsx
+++ src/sections/worked-on/works-content/index.jsx
@@ -1,2 +1,3 @@
 export { default as Santiamen } from "./santiamen";
 export { default as Halabalval } from "./halabalval";
+export { default as Hippopotamus } from "./hippopotamus"
