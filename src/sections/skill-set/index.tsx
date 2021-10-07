92e3ee0a543c7927dabc58efffd893ad4bb4cdb8
Diego Cardona <person.cardonadiego@gmail.com>
2021-10-07 11:50:38 -0500

    Fixed something realted to flex

--- src/sections/skill-set/index.tsx
+++ src/sections/skill-set/index.tsx
@@ -188,7 +188,7 @@ export default class extends PureComponent {
                     </div>
                 </div>
                 
-                <div className="flex flex-wrap space-x-4 space-y-4">
+                <div className="flex flex-wrap gap-4">
                     <PrintSkills skillSetData={skillSetData} />
                 </div>
             </div>
