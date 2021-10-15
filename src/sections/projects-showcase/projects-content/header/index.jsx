dfd3a8a9bb86668c7050da87609009ab9d0b3af3
Diego Cardona <person.cardonadiego@gmail.com>
2021-10-15 09:41:46 -0500

    Changed projects-content header image and also adding styles to it

--- src/sections/projects-showcase/projects-content/header/index.jsx
+++ src/sections/projects-showcase/projects-content/header/index.jsx
@@ -1,27 +1,25 @@
 import react from "react";
 
-//import "./index.css";
-
 export default function Header({ toggleShowLightbox, repo, thanks }) {
-    const projectHeaderBackground = { backgroundImage: `url(${"/static/images/sections/worked-on/header.jpg"})` };
+    const projectHeaderBackground = { backgroundImage: `url(${"/static/images/sections/projects/header.jpg"})` };
     
     return (
-        <div id="project-header" style={projectHeaderBackground}>
-            <div id="project-header__content">
-                <a id="project-header__repo" href={repo} target="_blank">
+        <div style={projectHeaderBackground} className="p-4 grid gap-4">
+            <div className="border border-black p-2">
+                <a className="p-2 bg-red-400 flex gap-2" href={repo} target="_blank">
                     <img src="/static/icons/github.svg" /> Repo
                 </a>
 
                 <div id="project-header__technologies-container">
                     Thanks to 
-                    <div id="project-header__technologies">
-                        { thanks.map(technology => <img src={`/static/icons/skills/${technology}.svg`} />) }
+                    <div className="flex gap-4">
+                        { thanks.map(technology => <img src={`/static/icons/skills/${technology}.svg`} className="w-8" />) }
                     </div>
                 </div>
             </div>
             
-            <div id="project-header__back-button" onClick={toggleShowLightbox}>
-                <img src="/static/icons/enter.svg" /> <b>back</b>
+            <div onClick={toggleShowLightbox} className="border border-black bg-blue-200 hover:bg-blue-300 py-1 px-4 flex gap-2 items-center">
+                <img src="/static/icons/enter.svg" className="w-4 transform rotate-180" /> <b>back</b>
             </div>
         </div>
     )
